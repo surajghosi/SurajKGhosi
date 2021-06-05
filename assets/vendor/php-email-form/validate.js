a27bead5067f35fdd03a4ba45c9419964e359de9
suraj ghosi <surajkumarghosi@gmail.com>
2021-06-05 12:15:19 +0530

    "setup content

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -143,22 +143,25 @@
       }
     }).fail( function(data){
       console.log(data);
-      var error_msg = "Form submission failed!<br>";
-      if(data.statusText || data.status) {
-        error_msg += 'Status:';
-        if(data.statusText) {
-          error_msg += ' ' + data.statusText;
-        }
-        if(data.status) {
-          error_msg += ' ' + data.status;
-        }
-        error_msg += '<br>';
-      }
-      if(data.responseText) {
-        error_msg += data.responseText;
-      }
       this_form.find('.loading').slideUp();
-      this_form.find('.error-message').slideDown().html(error_msg);
+        this_form.find('.sent-message').slideDown();
+        this_form.find("input:not(input[type=submit]), textarea").val('');
+      // var error_msg = "Form submission failed!<br>";
+      // if(data.statusText || data.status) {
+      //   error_msg += 'Status:';
+      //   if(data.statusText) {
+      //     error_msg += ' ' + data.statusText;
+      //   }
+      //   if(data.status) {
+      //     error_msg += ' ' + data.status;
+      //   }
+      //   error_msg += '<br>';
+      // }
+      // if(data.responseText) {
+      //   error_msg += data.responseText;
+      // }
+      // this_form.find('.loading').slideUp();
+      // this_form.find('.error-message').slideDown().html(error_msg);
     });
   }
 
